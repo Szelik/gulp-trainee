812021bff055915d42489c4c585ad460e66d6266
emzil <emzil@rambler.ru>
2018-03-10 18:53:58 +0300

    third

--- js/main.js
+++ js/main.js
@@ -5,6 +5,17 @@ $('.banner-slider').slick({
   fade: true,
   arrows: true,
   cssEase: 'linear',
-  prevArrow: '<span class="ion-arrow-left-c slick-arrow slick-prev"></span>',
-  nextArrow: '<span class="ion-arrow-right-c slick-arrow slick-next "></span>'
+  prevArrow: '<span class="fas fa-chevron-left slick-arrow slick-prev"></span>',
+  nextArrow: '<span class="fas fa-chevron-right slick-arrow slick-next "></span>'
+});
+
+$('.baking-slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 800,
+  fade: true,
+  arrows: true,
+  cssEase: 'linear',
+  prevArrow: '<span class="fas fa-chevron-left slick-arrow slick-prev"></span>',
+  nextArrow: '<span class="fas fa-chevron-right slick-arrow slick-next "></span>'
 });
